4048046b77542f8183ae22705005c4ae3185261e
chunglt98 <46279479+chunglt98@users.noreply.github.com>
2025-10-09 15:06:07 +0700

    Update [endpoint].js

--- api/webhook/[endpoint].js
+++ api/webhook/[endpoint].js
@@ -1,3 +1,7 @@
+console.log("✅ KV_REST_API_URL:", process.env.KV_REST_API_URL);
+console.log("✅ KV_REST_API_TOKEN exists:", !!process.env.KV_REST_API_TOKEN);
+
+
 process.env.KV_REST_API_URL = process.env.STORAGE_KV_REST_API_URL;
 process.env.KV_REST_API_TOKEN = process.env.STORAGE_KV_REST_API_TOKEN;
 process.env.KV_REST_API_READ_ONLY_TOKEN = process.env.STORAGE_KV_REST_API_READ_ONLY_TOKEN;
